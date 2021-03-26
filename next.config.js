6acbb2e4551b8f20ed86e11c0b741082333eca7f
efrolov <efrolov@zuzex.com>
2021-03-27 00:08:09 +0300

    [client]: change config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     includePaths: [path.join(__dirname, "styles")],
   },
   env: {
-    BASE_URL: "http//:printstores.ru/api",
+    BASE_URL: "http://printstores.ru/api",
     FILE_STATIC_URL: "http://printstores/api/storage",
     RECAPTCHA_SITE_KEY: "6LeirWEaAAAAAGOUF9yqR3CDeMYxF2T502fjHBiM",
   },
