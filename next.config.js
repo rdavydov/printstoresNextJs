fea20e4b32ab41bd261465c9e479964ab7937678
efrolov <efrolov@zuzex.com>
2021-03-27 16:51:18 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 const path = require("path");
-
+const dev = process.env.NODE_ENV.trim() !== "production";
 module.exports = {
   distDir: "build",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
   env: {
-    BASE_URL: "http://printstores.ru/api",
-    FILE_STATIC_URL: "http://printstores/api/storage",
+    BASE_URL: dev ? "https://localhost:3010/api" : "https://printstores.ru/api",
+    FILE_STATIC_URL: dev ? "https://localhost:3010/api/storage" : "https://printstores/api/storage",
     RECAPTCHA_SITE_KEY: "6LeirWEaAAAAAGOUF9yqR3CDeMYxF2T502fjHBiM",
   },
 };
