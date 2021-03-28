85d514a76641669b12936e18c6db7f68f5c4a11d
efrolov <efrolov@zuzex.com>
2021-03-28 16:57:15 +0300

    fix docker file

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ module.exports = {
     includePaths: [path.join(__dirname, "styles")],
   },
   env: {
-    BASE_URL: "http://localhost:3000/api",
-    FILE_STATIC_URL: "http://localhost:3000/api/storage",
-    // BASE_URL: dev ? "https://localhost:3010/api" : "https://printstores.ru/api",
-    // FILE_STATIC_URL: dev ? "https://localhost:3010/api/storage" : "https://printstores/api/storage",
+    // BASE_URL: "http://localhost:3000/api",
+    // FILE_STATIC_URL: "http://localhost:3000/api/storage",
+    BASE_URL: dev ? "https://localhost:3010/api" : "https://printstores.ru/api",
+    FILE_STATIC_URL: dev ? "https://localhost:3010/api/storage" : "https://printstores/api/storage",
     RECAPTCHA_SITE_KEY: "6LeirWEaAAAAAGOUF9yqR3CDeMYxF2T502fjHBiM",
   },
 };
