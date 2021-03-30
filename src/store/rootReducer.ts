3b1602c0d11c75162cd78081c647d165050e0072
efrolov <efrolov@zuzex.com>
2021-03-30 20:47:01 +0300

    [client]: fix error for build

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,6 +1,5 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import categorySlice from "./reducers/categoryReducer/categoryReducer";
-import catalogReducer from "../reducers/catalog/catalog";
 import cartCheckoutSlice from "./reducers/cart/checkout/checkout.reducer";
 import cartProductsSlice from "./reducers/cart/products/products.reducer";
 
@@ -10,7 +9,6 @@ export const rootReducer = combineReducers({
     checkout: cartCheckoutSlice.reducer,
   }),
   category: categorySlice.reducer,
-  catalog: catalogReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
